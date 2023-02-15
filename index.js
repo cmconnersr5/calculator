baa5a82fc0409929aab995382801c0b665788636
cmconnersr5 <121987232+cmconnersr5@users.noreply.github.com>
2023-02-15 17:48:29 -0500

    Add delete function, fix button layout

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ function appendNum(e){
     let current = `${currNum}`+ e.target.textContent;
     dispCurr.append(current);
     console.log(current);
-    
-
 };
 
 // when click an operator, change curNum to prevNum and 
@@ -30,10 +28,10 @@ operator.forEach(button => {
 function selectOp(e){
     let prevNum = dispCurr.textContent;
     selectedOp = e.target.outerText;
-    console.log(selectedOp)
+    console.log(selectedOp);
     dispPrev.append(prevNum, selectedOp);
     dispCurr.textContent= '';
-    return (selectedOp,prevNum)
+    return (selectedOp,prevNum);
 }
 
 
@@ -42,7 +40,7 @@ let equal = document.getElementById('equal');
 equal.addEventListener('click', operate);
 
 
-// assign nums from display and do the math
+// assign nums from display and do the math, rounding to 2 decimals
 function operate(){
     let num1 = parseFloat(dispPrev.textContent.slice(0,-1))
     let num2 = parseFloat(dispCurr.textContent)
@@ -50,8 +48,6 @@ function operate(){
     dispCurr.textContent = '';
     dispPrev.append(prevNum)
 
-
-    
     if (selectedOp === '+'){
         add(num1,num2);
     } else if (selectedOp === '-'){
@@ -61,29 +57,52 @@ function operate(){
     } else if (selectedOp === '/'){
         divide(num1,num2);
     }
-
-}
+};
 
 function add(num1,num2){
     result = num1+num2;
+    result = (Math.round((result+Number.EPSILON) * 100) / 100);
     dispPrev.textContent = '';
-    dispCurr.append(result)
+    dispCurr.append(result);
 };
 
 function sub(num1,num2){
     result = num1-num2;
+    result = (Math.round((result+Number.EPSILON) * 100) / 100);
     dispPrev.textContent = '';
-    dispCurr.append(result)
+    dispCurr.append(result);
 };
 
 function mult(num1,num2){
-  result = num1*num2;
-  dispPrev.textContent = '';
-  dispCurr.append(result)
+    result = num1*num2;
+    result = (Math.round((result+Number.EPSILON) * 100) / 100);
+    dispPrev.textContent = '';
+    dispCurr.append(result);
 };
 
 function divide(num1,num2){
-  result = num1/num2;
-  dispPrev.textContent = '';
-  dispCurr.append(result)
-};
+        result = num1/num2;
+        result = (Math.round((result+Number.EPSILON) * 100) / 100);
+        dispPrev.textContent = '';
+        dispCurr.append(result);
+    };
+
+//clears the screen
+let clear = document.getElementById('clear');
+clear.addEventListener('click', clearScreen);
+
+function clearScreen(){
+    dispCurr.textContent = '';
+    dispPrev.textContent = '';
+
+}
+
+// deletes previous entry
+let deleteBtn = document.getElementById('delete')
+deleteBtn.addEventListener('click', deleteIt)
+
+function deleteIt(){
+    let newDisp = dispCurr.textContent.slice(0,-1);
+    dispCurr.textContent=''
+    dispCurr.append(newDisp);
+}
