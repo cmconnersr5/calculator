72ab33fb9a5302b6cf7e57c1985b2a66acf6b066
cmconnersr5 <121987232+cmconnersr5@users.noreply.github.com>
2023-02-15 11:45:37 -0500

    Add display functionality

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
 # calculator
 Odin Project Calculator
-
-click numbers to assign num1
-save num1 to prev num
-show prev num in display
-click operand to assign operand
-display operand or highlight selected
-click numbers to assign num2
-save num2 to current num
-show current num in display
-hit enter to perform calculation
-display result in display
-click clear to clear display
--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+let prevNum = ''
+let currNum = ''
+let selectedOp = ''
+
+//when click a button add to currOP
+let dispCurr = document.querySelector('.currOp');
+let currentNum = document.createTextNode(`${currNum}`);
+let dispPrev = document.querySelector('.prevOp');
+
+const addNum = document.querySelectorAll('.btn');
+addNum.forEach(button => {
+    button.addEventListener('click', appendNum);
+});
+
+function appendNum(e){
+    dispCurr.append(`${currNum}`+ e.target.textContent);
+};
+
+// when click an operator, change curNum to prevNum and 
+// enter new currNum
+const operator = document.querySelectorAll('.op');
+operator.forEach(button => {
+    button.addEventListener('click', selectOp);
+});
+
+function selectOp(e){
+    let prevNum = dispCurr.textContent
+    let selectedOp = e.target.textContent;
+    dispPrev.append(prevNum, selectedOp);
+    dispCurr.textContent= '';
+    return selectedOp;
+}
+
+
+
+
+
+
+
+
+
 function add(){
     num1+num2
 }
@@ -14,14 +55,14 @@ function divide(){
     num1/num2
 };
 
-function operate(operator, num1,num2){
-    if (operator === '+'){
+function operate(selectedOp, num1,num2){
+    if (selectedOp === '+'){
         return add(num1,num2);
-    } else if (operator === '-'){
+    } else if (selectedOp === '-'){
         return sub(num1,num2);
-    } else if (operator === '*'){
+    } else if (selectedOp === '*'){
         return mult(num1,num2);
-    } else if (operator === '/'){
+    } else if (selectedOp === '/'){
         return divide(num1,num2);
     }
 
